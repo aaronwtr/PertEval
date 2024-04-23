8da6e0b73c17ac00716c0005c1d941bb0c5e8549
Aaron <aaronwenteler@gmail.com>
2024-04-23 18:19:01 +0100

    add spectra code

---
+++ src/utils/spectra/perturb.py
@@ -0,0 +1,49 @@
+from spectrae import Spectra, SpectraDataset
+
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from gears.pertdata import PertData
+
+
+class PerturbGraphData(SpectraDataset):
+    def parse(self, pert_data):
+        self.adata = pert_data.adata
+        self.control_expression = self.adata[self.adata.obs['condition'] == 'ctrl'].X.toarray().mean(axis=0)
+        return [p for p in self.adata.obs['condition'].unique() if p != 'ctrl']
+
+    def get_mean_logfold_change(self, perturbation):
+        perturbation_expression = self.adata[self.adata.obs['condition'] == perturbation].X.toarray().mean(axis=0)
+        logfold_change = np.nan_to_num(np.log2(perturbation_expression + 1) - np.log2(self.control_expression + 1))
+        return logfold_change
+
+    def sample_to_index(self, sample):
+        if not hasattr(self, 'index_to_sequence'):
+            print("Generating index to sequence")
+            self.index_to_sequence = {}
+            for i in tqdm(range(len(self))):
+                x = self.__getitem__(i)
+                self.index_to_sequence['-'.join(list(x))] = i
+
+        return self.index_to_sequence[sample]
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        perturbation = self.samples[idx]
+        return self.get_mean_logfold_change(perturbation)
+
+
+class SPECTRAPerturb(Spectra):
+    def spectra_properties(self, sample_one, sample_two):
+        return -np.linalg.norm(sample_one - sample_two)
+
+    def cross_split_overlap(self, train, test):
+        average_similarity = []
+
+        for i in test:
+            for j in train:
+                average_similarity.append(self.spectra_properties(i, j))
+
+        return np.mean(average_similarity)
