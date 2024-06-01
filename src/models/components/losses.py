9a5cef5e63410a6bf0e4907bb79e4362b41e1050
Aaron <aaronwenteler@gmail.com>
2024-06-01 20:25:56 +0100

    implement weighted RMSE

--- src/models/components/losses.py
+++ src/models/components/losses.py
@@ -6,10 +6,9 @@ class WeightedRMSELoss(nn.Module):
     def __init__(self):
         super(WeightedRMSELoss, self).__init__()
 
-    def forward(self, y_pred, y_true, x_input):
-        x = x_input[:, :x_input.shape[1] // 2]
-        pert_effect = torch.sqrt(torch.abs(y_true - x))
+    def forward(self, y_pred, y_true):
+        abs_pert_effect = torch.abs(y_true)
 
-        weights = nn.functional.softmax(pert_effect, dim=-1)
+        weights = nn.functional.softmax(abs_pert_effect, dim=-1)
 
-        return torch.mean(weights * (y_true - y_pred) ** 2)
+        return torch.sqrt(torch.mean(weights * (y_true - y_pred) ** 2))
