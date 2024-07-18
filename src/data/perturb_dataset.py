fixes for mse calculation and fm integration

src/data/perturb_dataset.py
@@ -114,7 +114,6 @@ class PerturbData(Dataset):
 
         pert_adata = sg_pert_adata[sg_pert_adata.obs['condition'] != 'ctrl', :]
         all_perts = list(set(pert_adata.obs['condition'].to_list()))
-        unique_perts = list(set(pert_list))
 
         num_cells = ctrl_adata.shape[0]
         num_perts = len(all_perts)
@@ -149,6 +148,9 @@ class PerturbData(Dataset):
                 fm_ctrl_data = pkl.load(f)
             with gzip.open(f"{self.data_path}/embeddings/{self.data_name}_{self.fm}_fm_pert.pkl.gz", "rb") as f:
                 fm_pert_data = pkl.load(f)
+
+            fm_pert_data = {pert: emb for pert, emb in fm_pert_data.items() if emb.shape[0] > 0}
+            unique_perts = list(fm_pert_data.keys())
 
             assert isinstance(fm_ctrl_data, (np.ndarray, anndata.AnnData)), ("fm_ctrl_data should be an array or an "
                                                                              "h5ad file!")
@@ -186,6 +188,7 @@ class PerturbData(Dataset):
                 sg_hvg_adata = sg_pert_adata[:, combined_genes]
 
                 pert_adata = sg_hvg_adata[sg_hvg_adata.obs['condition'] != 'ctrl', :]
+                pert_adata = pert_adata[pert_adata.obs['condition'].isin(unique_perts), :]
 
                 pert_adata.write(f"{self.data_path}/{self.data_name}_pp_pert_filtered.h5ad", compression='gzip')
 
@@ -224,8 +227,9 @@ class PerturbData(Dataset):
                     basal_ctrl_adata = anndata.AnnData(X=basal_ctrl_X_empty, obs=pert_adata.obs)
                     basal_ctrl_adata.obsm['X'] = basal_ctrl_X
 
-                    basal_ctrl_adata.write(f"{self.data_path}/embed_basal_ctrl_{self.data_name}_pp_filtered.h5ad",
-                                        compression='gzip')
+                    # basal_ctrl_adata.write(f"{self.data_path}/embed_basal_ctrl_{self.data_name}_pp_filtered.h5ad",
+                    #                    compression='gzip')
+                    basal_ctrl_adata.write(f"{self.data_path}/embed_basal_ctrl_{self.data_name}_pp_filtered.h5ad")
             with open(f"{self.data_path}/all_perts.pkl", "wb") as f:
                 pkl.dump(all_perts, f)
         else:

src/models/prediction_module.py
@@ -1,18 +1,15 @@
-# src/models/components/simple_module.py
-
-from typing import Any, Literal, Optional, Dict, Tuple
+from typing import Any, Literal, Optional, Dict, Tuple, List
 import torch
 import torch.nn as nn
 from lightning import LightningModule
 from torchmetrics import MeanSquaredError, MeanMetric
-from src.models.components.predictors import LinearRegressionModel, MLP
 
 
 class PredictionModule(LightningModule):
     def __init__(
             self,
             net: torch.nn.Module,
-            model_type: Literal["linear_regression", "mlp"] = "mlp",
+            model_type: Literal["mean", "linear_regression", "mlp"] = "mlp",
             optimizer: torch.optim.Optimizer = torch.optim.Adam,
             criterion: Optional[torch.nn.Module] = nn.MSELoss(),
             compile: Optional[bool] = False,
@@ -23,7 +20,8 @@ class PredictionModule(LightningModule):
         self.save_hyperparameters(logger=False)
 
         self.net = net
-        self.model_type = model_type  # saving placeholder in case different forward logic is required for different models
+        self.model_type = model_type    # saving placeholder in case different forward logic is required for different
+                                        # models
         self.criterion = criterion
         self.compile = compile
 
@@ -97,20 +95,14 @@ class PredictionModule(LightningModule):
             loss, preds, targets = self.model_step((x, y, input_expr))
             num_genes = len(de_idx)
             self.log("test/de_genes", num_genes, on_step=False, on_epoch=True, prog_bar=False)
-            mean_expr = torch.mean(input_expr, dim=0)
-            mean_expr = mean_expr.repeat(targets.shape[0], 1)
-            mean_eff = mean_expr - input_expr
             de_idx = torch.tensor([int(idx[0]) for idx in de_idx])
             de_idx = torch.tensor(de_idx)
             preds = preds[:, de_idx]
             targets = targets[:, de_idx]
-            self.baseline_mse(mean_eff[:, de_idx], targets)
-            self.log("test_baseline/mse", self.baseline_mse, on_step=False, on_epoch=True, prog_bar=False)
             self.test_mse(preds, targets)
             self.log("test_de/mse", self.test_mse, on_step=False, on_epoch=True, prog_bar=True)
         else:
             loss, preds, targets = self.model_step(batch)
-
             self.test_mse(preds, targets)
             self.log("test/mse", self.test_mse, on_step=False, on_epoch=True, prog_bar=True)
 