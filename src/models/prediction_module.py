98f03a3629cbdb783a8c6cf8e0e4db952b0fedee
Aaron <aaronwenteler@gmail.com>
2024-08-21 21:12:59 +0100

    cleanups and bugfixes prediction module

--- src/models/prediction_module.py
+++ src/models/prediction_module.py
@@ -14,11 +14,13 @@ class PredictionModule(LightningModule):
             criterion: Optional[torch.nn.Module] = nn.MSELoss(),
             compile: Optional[bool] = False,
             scheduler: Optional[torch.optim.lr_scheduler._LRScheduler] = None,
+            mean_adjusted: Optional[bool] = False
     ) -> None:
         super().__init__()
 
         self.save_hyperparameters(logger=False)
 
+        self.mean_adjusted = mean_adjusted
         self.net = net
         self.model_type = model_type    # saving placeholder in case different forward logic is required for different
                                         # models
@@ -40,12 +42,9 @@ class PredictionModule(LightningModule):
     # noinspection PyTupleAssignmentBalance
     def model_step(self, batch: Tuple[torch.Tensor, torch.Tensor, Optional[dict], Optional[torch.Tensor]]) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         if len(batch) == 4:
-            x, y, _, input_expr = batch
-        elif len(batch) == 3:
-            x, y, input_expr = batch
+            x, y, deg_dict, input_expr = batch
         else:
-            x, y = batch
-            input_expr = None
+            x, y, input_expr = batch
 
         if x.dtype != torch.float32:
             x = x.to(torch.float32)
@@ -53,9 +52,7 @@ class PredictionModule(LightningModule):
         if y.dtype != torch.float32:
             y = y.to(torch.float32)
 
-        if input_expr is None:
-            input_expr = x[:, :x.shape[1] // 2]
-        elif input_expr.dtype != torch.float32:
+        if input_expr.dtype != torch.float32:
             input_expr = input_expr.to(torch.float32)
 
         preds = self.forward(x)
@@ -84,32 +81,42 @@ class PredictionModule(LightningModule):
         if len(batch) == 4:
             x, y, de_dict, input_expr = batch
         elif len(batch) == 3:
-            x, y, input_expr = batch
+            x, y, _expr_or_de_dict = batch
+            if isinstance(_expr_or_de_dict, dict):
+                de_dict = _expr_or_de_dict
+                input_expr = x[:, :x.shape[1] // 2]
+            else:
+                input_expr = _expr_or_de_dict
         else:
             x, y = batch
-            input_expr = x[:, :x.shape[1] // 2]
+            # input_expr = x[:, :x.shape[1] // 2]
+            input_expr = x
         if not de_dict:
             loss, preds, targets = self.model_step((x, y, input_expr))
             self.test_mse(preds, targets)
             self.log("test/mse", self.test_mse, on_step=False, on_epoch=True, prog_bar=True)
         else:
             de_idx = de_dict['de_idx']
-            loss, preds, targets = self.model_step((x, y, input_expr))
+            loss, preds, targets = self.model_step((x, y, de_idx, input_expr))
             num_genes = len(de_idx)
-            mean_expr = torch.mean(input_expr, dim=0)
-            mean_expr = mean_expr.repeat(targets.shape[0], 1)
-            mean_eff = mean_expr - input_expr
             self.log("test/num_de_genes", num_genes, on_step=False, on_epoch=True, prog_bar=False)
             de_idx = torch.tensor([int(idx[0]) for idx in de_idx])
             de_idx = torch.tensor(de_idx)
             preds = preds[:, de_idx]
             targets = targets[:, de_idx]
-            self.baseline_mse(mean_eff[:, de_idx], targets)
-            self.test_mse(preds, targets)
-            baseline_mse_value = self.baseline_mse.compute()
-            test_mse_value = self.test_mse.compute()
-            adjusted_mse = test_mse_value - baseline_mse_value
-            self.log("test/adjusted_mse", adjusted_mse, on_step=False, on_epoch=True, prog_bar=True)
+            if self.mean_adjusted:
+                mean_expr = torch.mean(input_expr, dim=0)
+                mean_expr = mean_expr.repeat(targets.shape[0], 1)
+                mean_eff = mean_expr - input_expr
+                self.baseline_mse(mean_eff[:, de_idx], targets)
+                baseline_mse_value = self.baseline_mse.compute()
+                self.test_mse(preds, targets)
+                test_mse_value = self.test_mse.compute()
+                adjusted_mse = test_mse_value - baseline_mse_value
+                self.log("test/adjusted_mse", adjusted_mse, on_step=False, on_epoch=True, prog_bar=True)
+            else:
+                self.test_mse(preds, targets)
+                self.log("test/mse", self.test_mse, on_step=False, on_epoch=True, prog_bar=True)
 
     def setup(self, stage: str) -> None:
         """Lightning hook that is called at the beginning of fit (train + validate), validate,
